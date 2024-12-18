17273c5385d4617fd0b000375b4eaf8a29ec1b43
KZN-coder <deal_dragunov@mail.ru>
2024-12-18 17:43:29 +0300

    Alpha 0.3

--- api_hand.py
+++ api_hand.py
@@ -1,15 +1,92 @@
+import kaleido
+import plotly.graph_objects as go
 import requests
 import config
+from datetime import datetime
 
 API_KEY = f'{config.proxy_API_KEY}'
-url = f'https://api.dashboard.proxy.market/dev-api/v2/packages/{API_KEY}'
 
-data = {
+get_proxy_data = {
     "page": '1',
     "perPage": '20',
 }
 
 def get_data():
-    response = requests.get(url, json=data)
+    response = requests.get(f'https://api.dashboard.proxy.market/dev-api/v2/packages/{API_KEY}', json=get_proxy_data)
     responce_data = response.json()
+    print("Recieved new data")
+    print(response.status_code)
     return responce_data
+
+def get_proxy_lists(id):
+    get_proxy_lists_data = {
+        "type": "all",
+        "proxy_type": "resident",
+        "page": 1,
+        "page_size": 20,
+        "sort": 0,
+        "package_id": id
+    }
+    response = requests.post(f'https://api.dashboard.proxy.market/dev-api/list/{API_KEY}', json=get_proxy_lists_data)
+    response_data = response.json()
+    print("Recieved lists")
+    print(response.status_code)
+    return response_data
+
+def formating_proxy_lists(id):
+    data = get_proxy_lists(id)["list"]["data"]
+    output = ""
+    for item in data:
+        output+= f'{item["login"]}:{item["password"]}@{item["ip"]}:10000' + ','
+    return output
+
+def get_image():
+    data = get_data()
+    names = []
+    remaining = []
+    for item in data["data"]:
+        used = round(((int(item["used"])) / (1024 * 1024)), 2)
+        total = round((int(item["total"])) / (1024 * 1024), 2)
+        lasts = round(total - used, 2)
+        if lasts > 0:
+            names.append(item["name"] + ' (' + str(item["id"]) + ')')
+            remaining.append(lasts)
+    # Создание диаграммы
+    fig = go.Figure(go.Bar(
+        y=remaining,
+        x=names,
+        orientation='v',
+        marker=dict(color='#dbc360'),
+        text=remaining,  # Добавление текста на столбцы
+        textposition='inside',  # Позиция текста внутри столбцов
+        textfont=dict(color='black', size=20)  # Настройка шрифта
+    ))
+
+    fig.update_layout(
+        xaxis_title='Название списка',
+        yaxis_title='Оставшийся трафик (Mb)',
+        font=dict(color='black', size=20),
+        width=1920,  # Ширина изображения
+        height=1080  # Высота изображения
+    )
+    fig.write_image("./data/proxy_traffic.png", scale=3)
+
+def get_list():
+    output = "Available proxy list: \n\n-----\n\n"
+    data = get_data()
+    for item in data["data"]:
+        used = round(((int(item["used"])) / (1024 * 1024)), 2)
+        total = round((int(item["total"])) / (1024 * 1024), 2)
+        lasts = round(total - used, 2)
+        if lasts > 0:
+            output += "List: *" + item["name"] + "*" + '\n'
+            output += "Lasts: _" + str(lasts) + "Mb_" + '\n'
+            output += "Used: _" + str(used) + "Mb_" + " from _" + str(total) + "Mb_ \n"
+            output += "Expires at: " + convert_timestamp_to_date(int(item["expires_at"])) + '\n\n-----\n\n'
+    return output
+
+
+def convert_timestamp_to_date(timestamp):
+    date_object = datetime.fromtimestamp(timestamp)
+    formatted_date = date_object.strftime('%d %B %H:%M:%S')
+    return formatted_date
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import telebot
 import api_hand
 import config
-from datetime import datetime
+
 
 API_TOKEN = f'{config.bot_TOKEN}'
 bot = telebot.TeleBot(API_TOKEN)
@@ -10,24 +10,26 @@ bot = telebot.TeleBot(API_TOKEN)
 def start_answer(message):
     bot.send_message(message.chat.id, "I'm the bot to get actual proxy data from proxy.market")
 
+
 @bot.message_handler(commands=['proxies'])
-def send_welcome(message):
-    output = "Available proxy list: \n\n-----\n\n"
-    data = api_hand.get_data()
-    for item in data["data"]:
-        used = round(((int(item["used"])) / (1024 * 1024)), 2)
-        total = round((int(item["total"])) / (1024 * 1024), 2)
-        lasts = round(total - used, 2)
-        if lasts>0:
-            output += "List: *" + item["name"]+"*" + '\n'
-            output += "Lasts: _" + str(lasts) + "Mb_" + '\n'
-            output += "Used: _" + str(used) + "Mb_" + " from _" + str(total) + "Mb_ \n"
-            output+= "Expires at: " + convert_timestamp_to_date(int(item["expires_at"])) + '\n\n-----\n\n'
+def send_list(message):
+    output = api_hand.get_list()
     bot.send_message(message.chat.id, output, parse_mode='Markdown')
 
-def convert_timestamp_to_date(timestamp):
-    date_object = datetime.fromtimestamp(timestamp)
-    formatted_date = date_object.strftime('%d %B %H:%M:%S')
-    return formatted_date
+
+@bot.message_handler(commands=['image'])
+def send_image(message):
+    api_hand.get_image()
+    with open('./data/proxy_traffic.png', 'rb') as photo:
+        bot.send_photo(message.chat.id, photo=photo)
+
+
+@bot.message_handler(commands=['getlist'])
+def handle_command(message):
+    text = message.text
+    parts = text.split(' ', 1)
+    command, params = parts
+    bot.send_message(message.chat.id, api_hand.formating_proxy_lists(params))
+
 
 bot.infinity_polling()
